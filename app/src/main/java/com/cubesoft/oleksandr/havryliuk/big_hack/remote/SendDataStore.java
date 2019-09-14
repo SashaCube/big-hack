4d30191a25de16d10adef278dcb1cb308eaa26be
Bodka <kroxmaluker@gmail.com>
2019-09-14 22:45:20 +0300

    SendDataStore

---
+++ app/src/main/java/com/cubesoft/oleksandr/havryliuk/big_hack/remote/SendDataStore.java
@@ -0,0 +1,26 @@
+package com.cubesoft.oleksandr.havryliuk.big_hack.remote;
+
+import com.cubesoft.oleksandr.havryliuk.big_hack.data.model.Answer;
+import com.cubesoft.oleksandr.havryliuk.big_hack.data.model.Mark;
+import com.cubesoft.oleksandr.havryliuk.big_hack.data.model.Task;
+
+import org.jetbrains.annotations.NotNull;
+
+public class SendDataStore implements ISendDataStore {
+
+
+    @Override
+    public void sendTask(@NotNull Task task) {
+
+    }
+
+    @Override
+    public void sendMark(@NotNull Mark mark) {
+
+    }
+
+    @Override
+    public void sendAnswer(@NotNull Answer answer) {
+
+    }
+}
