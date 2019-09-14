73d605688f3cf3e43d476c83ead8e2a4a736b56a
Bodka <kroxmaluker@gmail.com>
2019-09-14 22:28:51 +0300

    changes Test

--- app/src/main/java/com/cubesoft/oleksandr/havryliuk/big_hack/remote/Test.java
+++ app/src/main/java/com/cubesoft/oleksandr/havryliuk/big_hack/remote/Test.java
@@ -7,21 +7,52 @@ import com.bloqly.api.KeyPair;
 import com.bloqly.api.transaction.SignedTransaction;
 import com.bloqly.api.transaction.Transaction;
 
+
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class Test {
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public void testTransaction() {
-        String privateKey = "giwyrQcDYhudVrSjdKLgp44YwyD/nl3F1AnO0KKeJTo=";
-        String publicKey = "sveQDMN3Fdzi0Yvm3bhkyu0uT/08wQfymhXqD0GT4vU=";
-        String space = "space1";
-        String key = "key1";
 
-        BloqlyClient bloqlyClient = new BloqlyClient("http://10.4.32.17:8082");
+    public static final String TEATCHER = "TEATCHER";
+    public static final String SUBJECT = "SUBJECT";
+    public static final String STUDENT = "STUDENT";
+    public static final String TASK = "TASK";
+    public static final String ANSWER = "ANSWER";
+    public static final String CLASS = "CLASS";
+
+
+    String privateKey = "giwyrQcDYhudVrSjdKLgp44YwyD/nl3F1AnO0KKeJTo=";
+    String publicKey = "sveQDMN3Fdzi0Yvm3bhkyu0uT/08wQfymhXqD0GT4vU=";
+    String space = "space1";
+    String key = "key1";
+    BloqlyClient bloqlyClient;
+
+
+    public void main(){
+        List<String> tags = new ArrayList<String>();
+
+        tags.add("Thisiscustomtag3");
+        tags.add("Thisiscustomtag2");
+        tags.add("Thisiscustomtag1");
+
+
+        sighnAndSendTransaction(privateKey,space,key,tags,"This is memo2","EMPTY VAL");
+    }
+
+    public Test(){
+        bloqlyClient = new BloqlyClient("http://10.4.32.17:8082");
+    }
+
+    public void sendTask(String task){
+
+    }
+
+    public void sighnAndSendTransaction(String privateKey, String space,
+                         String key, List<String> tags, String memo, String value){
 
         Optional<SignedTransaction> txOpt = bloqlyClient.getLastTransaction(space, key);
 
@@ -39,17 +70,22 @@ public class Test {
         tx.setSpace(space);
         tx.setKey(key);
         tx.setNonce(nonce);
-        tx.setMemo("memo");
+        tx.setMemo(memo);
         tx.setTimestamp(Instant.now().toEpochMilli());
-        tx.setValue("test-value");
-
-        List<String> tags = new ArrayList<>();
-        tags.add("tag1");
-        tags.add("tag2");
+        tx.setValue(value);
         tx.setTags(tags);
+
+/*      List<String> tags = new ArrayList<>();
+        tags.add("tag1");
+        tags.add("tag2");*/
+
 
         SignedTransaction signedTx = keyPair.signTransaction(tx);
 
         bloqlyClient.submitTransaction(signedTx);
     }
+
+
+
+
 }
